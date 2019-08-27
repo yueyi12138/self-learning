9be5e29b23b913f2c78a26d5394ab096513de9aa
Yue <1298751579@qq.com>
2019-08-27 17:08:40 +0800

    算法练习

---
+++ 2019.8.25/2019.8.25/test.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int IsPowTow2(int n)
+{
+	int m = n&(n - 1);
+	if (n < 1)
+	{
+		return 0;
+	}
+	if (i == n)
+	{
+		return 1;
+	}
+}
+int IsPowTow(int n)
+{
+	int i = 0;
+	for (i = 1; i <= n; i <<= 1)
+	{
+		if (i == n)
+		{
+			return 1;
+		}
+	}
+}
+int main()
+{
+
+	return 0;
+}
